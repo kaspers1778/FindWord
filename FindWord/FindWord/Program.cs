729e0457863a3fcab8437ba922b5d0a6e7337f6a
Pete Balachin <pet.balachin1778@gmail.com>
2019-03-05 12:29:45 +0200

    Create project

---
+++ FindWord/FindWord/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
