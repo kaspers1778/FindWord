c2254ac0e659036fe6f11cdac73d20d3f870296b
Nikita Pedorenko <nikitnya1@gmail.com>
2019-03-05 14:47:13 +0200

    Add ColoredWrite

---
+++ FindWord/FindWord/Coloring.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace FindWord
+{
+
+    static class Coloring
+    {
+        static private Random rand = new Random();
+        static private Array values = Enum.GetValues(typeof(ConsoleColor));
+
+        static public void ColoredWriteLine(ConsoleColor color, string value)
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ForegroundColor = prevColor;
+        }
+
+        static public void ColoredWrite(ConsoleColor color, string value)
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(value);
+            Console.ForegroundColor = prevColor;
+        }
+
+        static public void RandomColoredWriteLine(string value)
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = (ConsoleColor)values.GetValue(rand.Next(values.Length));
+            Console.WriteLine(value);
+            Console.ForegroundColor = prevColor;
+        }
+
+        static public void RandomColoredWrite(string value)
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = (ConsoleColor)values.GetValue(rand.Next(values.Length));
+            Console.Write(value);
+            Console.ForegroundColor = prevColor;
+        }
+    }
+}
