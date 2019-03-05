ff32b8e3f0bb7f050e9957574baadf2e8f4d2f31
Nikita Pedorenko <nikitnya1@gmail.com>
2019-03-05 13:26:02 +0200

    Add rand to Generator

--- FindWord/FindWord/Generator.cs
+++ FindWord/FindWord/Generator.cs
@@ -6,6 +6,7 @@ namespace FindWord
     static class Generator
     {
         static Random rand = new Random();
+
         private static string[] letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
         
         public static string GenerateWord(int length)
