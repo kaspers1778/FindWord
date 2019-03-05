b2f745db37c3223472653e50c0bef8dd2aaccea8
Nikita Pedorenko <nikitnya1@gmail.com>
2019-03-05 13:04:29 +0200

    Created Generator class

---
+++ FindWord/FindWord/Generator.cs
@@ -0,0 +1,19 @@
+﻿namespace FindWord
+{
+    
+    static class Generator
+    {
+        private static string[] letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        
+        public static string GenerateWord(int length)
+        {
+            string word = "";
+            for (int i = 0; i < length; i++)
+            {
+                word += letters[rand.Next(0, letters.Length - 1)];
+            }
+            return word;
+        }
+
+    }
+}
