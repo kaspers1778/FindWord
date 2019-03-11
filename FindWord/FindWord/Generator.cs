a3f4e95df35f540f9e15324546e2af70ec4b4b2d
Nikita Pedorenko <nikitnya1@gmail.com>
2019-03-11 15:39:40 +0200

    Update Generator.cs
    Oleg comments

--- FindWord/FindWord/Generator.cs
+++ FindWord/FindWord/Generator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace FindWord
 {
@@ -7,16 +8,19 @@ namespace FindWord
     {
         static Random rand = new Random();
 
+        //Use char and converting int to chars instead of an array of letters
         private static string[] letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
         
         public static string GenerateWord(int length)
         {
-            string word = "";
+            //Use string builder
+            StringBuilder word = new StringBuilder(length);
+            //string word = "";
             for (int i = 0; i < length; i++)
             {
-                word += letters[rand.Next(0, letters.Length - 1)];
+                word.Append(letters[rand.Next(0, letters.Length - 1)]);
             }
-            return word;
+            return word.ToString();
         }
 
     }
